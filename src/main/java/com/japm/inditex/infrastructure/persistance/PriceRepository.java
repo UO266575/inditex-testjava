51aaf3f92908366b815287254f6c18cd7cce6052
tonpm14 <uo266575@uniovi.es>
2025-02-13 13:56:45 +0100

    Adding persistence layer

--- src/main/java/com/japm/inditex/infrastructure/persistance/PriceRepository.java
+++ src/main/java/com/japm/inditex/infrastructure/persistance/PriceRepository.java
@@ -1,2 +1,23 @@
-package com.japm.inditex.infrastructure.persistance;public interface PriceRepository {
+package com.japm.inditex.infrastructure.persistance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface PriceRepository extends JpaRepository<PriceRepositoryEntity, Long> {
+
+    @Query("""
+                SELECT p FROM PriceEntity p
+                WHERE p.productId = :productId
+                AND p.brandId = :brandId
+                AND :applicationDate BETWEEN p.startDate AND p.endDate
+                ORDER BY p.priority DESC
+                LIMIT 1
+            """)
+    Optional<PriceRepositoryEntity> findPriceByProductIdBrandIdAndDate(
+            Long productId, Long brandId, LocalDateTime applicationDate);
 }
--- src/main/java/com/japm/inditex/infrastructure/persistance/PriceRepositoryEntity.java
+++ src/main/java/com/japm/inditex/infrastructure/persistance/PriceRepositoryEntity.java
@@ -1,2 +1,28 @@
-package com.japm.inditex.infrastructure.persistance;public class PriceRepositoryEntity {
+package com.japm.inditex.infrastructure.persistance;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Prices")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PriceRepositoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer priceList;
+    private Long productId;
+    private Integer priority;
+    private BigDecimal price;
+    private String currency;
 }
